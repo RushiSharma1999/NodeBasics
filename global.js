7af519fd27bffe8e7053fa9081852658b1eccf7a
Rushi Sharma <rushisharma64@gmail.com>
2023-08-28 16:01:25 -0700

    Global functionalities and exporting

---
+++ global.js
@@ -0,0 +1,15 @@
+//Global object
+
+//console.log(global);
+
+// global.setTimeout(() => {
+//     console.log("In the timeout");
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log("In the interval");
+// });
+
+console.log(__dirname);
+console.log(__filename);
---
+++ test.js
@@ -0,0 +1,10 @@
+// const name = "Rushi";
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`Hello, ${name}`);
+};
+
+greet("Rushi");
+greet("Lisa");
