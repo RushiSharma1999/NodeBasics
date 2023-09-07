0a35be1ab25031a9810b6d96162967e1afbf82ef
Rushi Sharma <rushisharma64@gmail.com>
2023-09-06 17:27:20 -0700

    Reading HTML pages on the server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 //creating a server
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write("<head><link rel='stylesheet' href='#' /></head>");
+    // res.write("<p>Hello there!</p>");
+    // res.write("<p>Hello again!</p>");
+    // res.end();
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 //listening to the server
